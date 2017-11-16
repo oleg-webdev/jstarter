fc33787c49876ca42a285d58a331e5fe30e33b9d
oleg-webdev <cibulnicoleg@gmail.com>
2017-11-16 21:22:03 +0200

    imports

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,14 @@
 import '../css/style.scss';
 import 'jquery';
 import 'bootstrap';
+
+import {externalConst} from './modules/externalModule';
+
+{
+	let testFunc = () => {
+		console.log(externalConst());
+	};
+
+	testFunc();
+	
+}
---
+++ src/js/modules/externalModule.js
@@ -0,0 +1,3 @@
+export const externalConst = () => {
+		return 'some string';
+}
