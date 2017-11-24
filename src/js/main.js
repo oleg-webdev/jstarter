f39a5fcb398d5fa4429fe3d9297171313efd804d
oleg-webdev <cibulnicoleg@gmail.com>
2017-11-24 14:33:56 +0200

    bootstrap and popper

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,27 @@
 import '../css/style.scss';
 import 'jquery';
-import 'bootstrap';
+// import Popper from 'popper.js';
+// window.Popper = Popper;
+// import 'bootstrap';
+
 
 import {externalConst} from './modules/externalModule';
 
-{
-	let testFunc = () => {
-		console.log(externalConst());
-	};
-
-	testFunc();
+let testFunc = () => {
 	
+	console.log(this);
+};
+
+let someperson = new testFunc();
+
+function someFunc() {
+	let that = this
+	that.name = 'Alice';
+
+	let testFunc = function() {
+		console.log(that.name);
+	};
+	testFunc();
 }
+
+let person = new someFunc();
