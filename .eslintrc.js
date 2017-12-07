c9d1b6a31aeebdabeca3f1e4ced992478d3d2116
oleg-webdev <cibulnicoleg@gmail.com>
2017-12-07 16:30:33 +0200

    master branch starter template

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,12 @@ module.exports = {
     "no-new": 0,
     "no-console": 0,
     'arrow-body-style': 0,
+    // "react/prefer-stateless-function": 0,
+    "react/no-unescaped-entities": 0,
     "func-names": ["error", "never"],
     "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
   },
+  "parser": "babel-eslint",
   "globals": {
     "window": true,
     "document": true,
--- src/js/modules/ExternalModule.jsx
+++ src/js/modules/ExternalModule.jsx
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 
 class ExternalModule extends Component {
+  state = { showText: true };
 
   render() {
     return (
       <div>
-        <h3>Hi, I'm external</h3>
+        <h3>Hi, I'm external {this.state.showText}</h3>
       </div>
-    )
+    );
   }
-
 }
 
 export default ExternalModule;
